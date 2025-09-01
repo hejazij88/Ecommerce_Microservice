e7b6d00c65ec5101e43af429d432e3f1595ccbaf
98913 <98913@LAPTOP-JQR66TAF>
2025-09-01 12:03:33 +0330

    Add ProductDto

---
+++ Src/Services/Catalog/Catalog.Api/Dtos/Product DTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.Api.Dtos;
+
+public class Product_DTO
+{
+    public record ProductDto(string Id, string Name, string Description, decimal Price, int Stock);
+
+
+    public record CreateProductDto(string Name, string Description, decimal Price, int Stock);
+}
--- Src/Services/Catalog/Catalog.Applications/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Catalog.Applications
-{
-    public class Class1
-    {
-
-    }
-}
