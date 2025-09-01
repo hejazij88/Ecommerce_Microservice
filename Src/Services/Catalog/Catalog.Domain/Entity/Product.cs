e493850d69f265d24034e05230a7a0909d123aa8
98913 <98913@LAPTOP-JQR66TAF>
2025-09-01 11:19:49 +0330

    Add Product Entity

---
+++ Src/Services/Catalog/Catalog.Domain/Entity/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalog.Domain.Entity;
+
+public class Product
+{
+    public string Id { get; set; } = null;
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+}
