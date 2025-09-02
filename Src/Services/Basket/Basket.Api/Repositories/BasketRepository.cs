11deaeb58d9e0ea3bb6103ec063f5936be4803c4
98913 <98913@LAPTOP-JQR66TAF>
2025-09-02 16:14:29 +0330

    Add Basket Repository

---
+++ Src/Services/Basket/Basket.Api/Repositories/BasketRepository.cs
@@ -0,0 +1,27 @@
+﻿using Basket.Api.Entity;
+
+namespace Basket.Api.Repositories;
+
+public class BasketRepository:IBasketRepository
+{
+
+    private static readonly Dictionary<string, ShoppingCart> _storage = new();
+    public async Task<ShoppingCart> GetBasket(string userName)
+    {
+        if (!_storage.TryGetValue(userName, out var basket))
+            basket = new ShoppingCart(userName);
+
+        return await Task.FromResult(basket);
+    }
+
+    public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+    {
+        _storage[basket.UserName] = basket;
+        return await Task.FromResult(basket);
+    }
+
+    public async Task DeleteBasket(string userName)
+    {
+        _storage.Remove(userName);
+    }
+}
