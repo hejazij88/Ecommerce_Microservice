0d2413486a5176125e0a84bad88134b08ab2677e
98913 <98913@LAPTOP-JQR66TAF>
2025-09-02 16:22:00 +0330

    Add RabitMqSetting in Basket

---
+++ Src/Services/Basket/Basket.Api/Message/RabbitMqSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace Basket.Api.Message;
+
+public class RabbitMqSettings
+{
+    public string HostName { get; set; } = "localhost";
+    public int Port { get; set; } = 5672;
+    public string UserName { get; set; } = "guest";
+    public string Password { get; set; } = "guest";
+    public string ExchangeName { get; set; } = "event_bus"; 
+    public string ExchangeType { get; set; } = "topic"; 
+    public bool DevBindQueue { get; set; } = true; 
+    public string DevQueueName { get; set; } = "dev_basket_checkout_q";
+    public string RoutingKey { get; set; } = "basket.checkout";
+}
