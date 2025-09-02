310480179451c32b2177478087d0f2e361ed8588
98913 <98913@LAPTOP-JQR66TAF>
2025-09-02 16:20:38 +0330

    Add BasketCheckoutEvent

---
+++ Src/Services/Basket/Basket.Api/Events/BasketCheckoutEvent.cs
@@ -0,0 +1,24 @@
+﻿namespace Basket.Api.Events
+{
+    public class BasketCheckoutEvent
+    {
+        public string EventId { get; set; } = Guid.NewGuid().ToString();
+        public string EventType { get; set; } = "BasketCheckedOut";
+        public string OccurredAt { get; set; } = DateTime.UtcNow.ToString("O");
+        public string Version { get; set; } = "1";
+        public string CorrelationId { get; set; } = Guid.NewGuid().ToString();
+
+
+        public string UserName { get; set; } = default!;
+        public decimal TotalPrice { get; set; }
+
+
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string EmailAddress { get; set; } = string.Empty;
+        public string AddressLine { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string ZipCode { get; set; } = string.Empty;
+    }
+}
