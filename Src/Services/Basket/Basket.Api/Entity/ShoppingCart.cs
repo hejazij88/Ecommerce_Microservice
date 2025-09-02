903c5b130c9579c97211ca1c3363e45e0af891d0
98913 <98913@LAPTOP-JQR66TAF>
2025-09-02 16:10:27 +0330

    Add Basket Entity

---
+++ Src/Services/Basket/Basket.Api/Entity/ShoppingCart.cs
@@ -0,0 +1,10 @@
+﻿namespace Basket.Api.Entity;
+
+public class ShoppingCart
+{
+    public string UserName { get; set; } = default!;
+    public List<ShoppingCartItem> Items { get; set; } = new();
+    public ShoppingCart() { }
+    public ShoppingCart(string userName) => UserName = userName;
+    public decimal TotalPrice => Items.Sum(i => i.Price * i.Quantity);
+}
