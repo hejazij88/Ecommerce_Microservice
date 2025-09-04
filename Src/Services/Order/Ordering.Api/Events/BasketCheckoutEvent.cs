08d74925c5470723ae68001ecdc925ac212ef58e
98913 <98913@LAPTOP-JQR66TAF>
2025-09-04 13:38:08 +0330

    Add Event model

---
+++ Src/Services/Order/Ordering.Api/Events/BasketCheckoutEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace Ordering.Api.Events;
+
+public class BasketCheckoutEvent
+{
+    public string UserName { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string EmailAddress { get; set; } = string.Empty;
+    public string AddressLine { get; set; } = string.Empty;
+    public string Country { get; set; } = string.Empty;
+    public string State { get; set; } = string.Empty;
+    public string ZipCode { get; set; } = string.Empty;
+    public decimal TotalPrice { get; set; }
+}
