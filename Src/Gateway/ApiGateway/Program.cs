1ccfc423fc519b9b8decf101226d919974a62027
98913 <98913@LAPTOP-JQR66TAF>
2025-09-04 15:34:01 +0330

    Add Authorization In Gateway

--- Src/Gateway/ApiGateway/Program.cs
+++ Src/Gateway/ApiGateway/Program.cs
@@ -1,10 +1,40 @@
-using Yarp.ReverseProxy;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// JWT Auth
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = "eshop-gateway",
+            ValidAudience = "eshop-users",
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("SuperSecretKey_12345"))
+        };
+    });
+
+builder.Services.AddAuthorization();
+
+// YARP Config
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
+
+app.UseAuthentication();  
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("default", policy =>
+        policy.RequireAuthenticatedUser());
+});
+
 app.MapReverseProxy();
+
 app.Run();
