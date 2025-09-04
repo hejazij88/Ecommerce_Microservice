13ebdcb5ce5fc4bc1b6c9627e1876ee5ece2df59
98913 <98913@LAPTOP-JQR66TAF>
2025-09-04 13:26:00 +0330

    Update Repository In Basket

--- Src/Services/Basket/Basket.Api/Repositories/BasketRepository.cs
+++ Src/Services/Basket/Basket.Api/Repositories/BasketRepository.cs
@@ -1,27 +1,39 @@
 ﻿using Basket.Api.Entity;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
 
 namespace Basket.Api.Repositories;
 
 public class BasketRepository:IBasketRepository
 {
 
-    private static readonly Dictionary<string, ShoppingCart> _storage = new();
-    public async Task<ShoppingCart> GetBasket(string userName)
-    {
-        if (!_storage.TryGetValue(userName, out var basket))
-            basket = new ShoppingCart(userName);
+    private readonly IDistributedCache _redisCache;
 
-        return await Task.FromResult(basket);
+    public BasketRepository(IDistributedCache redisCache)
+    {
+        _redisCache = redisCache;
+    }
+
+    public async Task<ShoppingCart?> GetBasket(string userName)
+    {
+        var basket = await _redisCache.GetStringAsync(userName);
+        if (string.IsNullOrEmpty(basket))
+            return null;
+
+        return JsonSerializer.Deserialize<ShoppingCart>(basket);
     }
 
     public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
     {
-        _storage[basket.UserName] = basket;
-        return await Task.FromResult(basket);
+        await _redisCache.SetStringAsync(
+            basket.UserName,
+            JsonSerializer.Serialize(basket));
+
+        return basket;
     }
 
     public async Task DeleteBasket(string userName)
     {
-        _storage.Remove(userName);
+        await _redisCache.RemoveAsync(userName);
     }
 }
--- Src/Services/Basket/Basket.Api/Repositories/IBasketRepository.cs
+++ Src/Services/Basket/Basket.Api/Repositories/IBasketRepository.cs
@@ -1,10 +1,11 @@
 ﻿using Basket.Api.Entity;
+using System.Threading.Tasks;
 
 namespace Basket.Api.Repositories;
 
 public interface IBasketRepository
 {
-    Task<ShoppingCart> GetBasket(string userName);
+    Task<ShoppingCart?> GetBasket(string userName);
     Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
     Task DeleteBasket(string userName);
 }
