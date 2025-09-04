cfc854772201d031bc551f213f88e6ca96cea755
98913 <98913@LAPTOP-JQR66TAF>
2025-09-04 13:44:49 +0330

    Config Program.cs order api

--- Src/Services/Order/Ordering.Api/Program.cs
+++ Src/Services/Order/Ordering.Api/Program.cs
@@ -1,17 +1,55 @@
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Api.Consumers;
+using Ordering.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+
+// PostgreSQL
+builder.Services.AddDbContext<OrderContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("OrderConnection")));
+
+// MassTransit (RabbitMQ)
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumer<BasketCheckoutConsumer>();
+    x.UsingRabbitMq((ctx, cfg) =>
+    {
+        cfg.Host("rabbitmq", "/", h =>
+        {
+            h.Username("guest");
+            h.Password("guest");
+        });
+
+        cfg.ReceiveEndpoint("order-service", e =>
+        {
+            e.ConfigureConsumer<BasketCheckoutConsumer>(ctx);
+        });
+    });
+});
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Health check
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("OrderConnection"));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.MapControllers();
+app.MapHealthChecks("/health");
 
 app.Run();
