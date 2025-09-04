a5a314db42d678e0a01023a58d5d1411e08d08e2
98913 <98913@LAPTOP-JQR66TAF>
2025-09-04 13:39:47 +0330

    Add Consumer Order

---
+++ Src/Services/Order/Ordering.Api/Consumers/BasketCheckoutConsumer.cs
@@ -0,0 +1,37 @@
+﻿using MassTransit;
+using Ordering.Api.Data;
+using Ordering.Api.Entity;
+using Ordering.Api.Events;
+
+namespace Ordering.Api.Consumers;
+
+public class BasketCheckoutConsumer: IConsumer<BasketCheckoutEvent>
+{
+    private readonly OrderContext _context;
+
+    public BasketCheckoutConsumer(OrderContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+    {
+        var message = context.Message;
+
+        var order = new Order
+        {
+            UserName = message.UserName,
+            FirstName = message.FirstName,
+            LastName = message.LastName,
+            EmailAddress = message.EmailAddress,
+            AddressLine = message.AddressLine,
+            Country = message.Country,
+            State = message.State,
+            ZipCode = message.ZipCode,
+            TotalPrice = message.TotalPrice
+        };
+
+        _context.Orders.Add(order);
+        await _context.SaveChangesAsync();
+    }
+}
