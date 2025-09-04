9efaf88436a01ddf704e232a82b65bd9f1dd62ec
98913 <98913@LAPTOP-JQR66TAF>
2025-09-04 13:36:10 +0330

    Add Order Entity

---
+++ Src/Services/Order/Ordering.Api/Entity/Order.cs
@@ -0,0 +1,16 @@
+﻿namespace Ordering.Api.Entity;
+
+public class Order
+{
+    public int Id { get; set; }
+    public string UserName { get; set; } = string.Empty;
+    public decimal TotalPrice { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string EmailAddress { get; set; } = string.Empty;
+    public string AddressLine { get; set; } = string.Empty;
+    public string Country { get; set; } = string.Empty;
+    public string State { get; set; } = string.Empty;
+    public string ZipCode { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
